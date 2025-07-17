b61b18bbf1dcff8bcf025a88c281f737209280ee
krishna-kumar <krishna537428@gmail.com>
2025-07-17 14:37:57 +0530

    Initial Commit

---
+++ RestaurantApp/RestaurantAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantAppApp.swift
+//  RestaurantApp
+//
+//  Created by KRISHNA on 17/07/25.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
